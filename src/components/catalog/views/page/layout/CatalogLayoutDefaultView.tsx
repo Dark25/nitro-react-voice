98a375a480696ab099f20c6300a970be234bac88
Lucifer <nadiecaca2000@gmail.com>
2022-09-29 02:11:02 +0200

    tab icons, navigation indent, catalog headers

--- src/components/catalog/views/page/layout/CatalogLayoutDefaultView.tsx
+++ src/components/catalog/views/page/layout/CatalogLayoutDefaultView.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react';
+import { GetConfiguration, ProductTypeEnum } from '../../../../../api';
 import { Base, Column, Flex, Grid, Text } from '../../../../../common';
 import { useCatalog } from '../../../../../hooks';
+import { CatalogHeaderView } from '../../catalog-header/CatalogHeaderView';
 import { CatalogAddOnBadgeWidgetView } from '../widgets/CatalogAddOnBadgeWidgetView';
 import { CatalogItemGridWidgetView } from '../widgets/CatalogItemGridWidgetView';
 import { CatalogLimitedItemWidgetView } from '../widgets/CatalogLimitedItemWidgetView';
@@ -13,20 +15,22 @@ import { CatalogLayoutProps } from './CatalogLayout.types';
 export const CatalogLayoutDefaultView: FC<CatalogLayoutProps> = props =>
 {
     const { page = null } = props;
-    const { currentOffer = null } = useCatalog();
+    const { currentOffer = null,currentPage } = useCatalog();
 
     return (
-        <Grid>
-            <Column size={ 7 } overflow="hidden">
-                <CatalogItemGridWidgetView />
-            </Column>
-            <Column center={ !currentOffer } size={ 5 } overflow="hidden">
-                { !currentOffer &&
+        <>
+            <Grid>
+                <Column size={ 7 } overflow="hidden">
+                    { GetConfiguration('catalog.headers') && <CatalogHeaderView image={ currentPage.localization.getImage(0) }/> }
+                    <CatalogItemGridWidgetView />
+                </Column>
+                <Column center={ !currentOffer } size={ 5 } overflow="hidden">
+                    { !currentOffer &&
                     <>
                         { !!page.localization.getImage(1) && <img alt="" src={ page.localization.getImage(1) } /> }
                         <Text style={{color: "var(--test-galaxytext)"}} variant="none" center dangerouslySetInnerHTML={ { __html: page.localization.getText(0) } } />
                     </> }
-                { currentOffer &&
+                    { currentOffer &&
                     <>
                         <Base position="relative" overflow="hidden">
                             <CatalogViewProductWidgetView />
@@ -44,7 +48,8 @@ export const CatalogLayoutDefaultView: FC<CatalogLayoutProps> = props =>
                             <CatalogPurchaseWidgetView />
                         </Column>
                     </> }
-            </Column>
-        </Grid>
+                    </Column>
+            </Grid>
+        </>
     );
 }
